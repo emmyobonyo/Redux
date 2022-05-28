319fbbc8cc33363f64cb3a710342b9e7389cb2b2
Emmanuel Obonyo <emmyobonyo@gmail.com>
2022-05-28 19:35:39 +0300

    random commit

---
+++ count.js
@@ -0,0 +1,17 @@
+function changeCount(amount = 1) {
+    return {
+        type: "CHANGE_COUNT",
+        payload: amount
+    }
+}
+
+function reducer (count = 0, action) {
+    switch(action.type) {
+        case "CHANGE_COUNT":
+            return count + action.payload
+        default:
+            return count
+    }
+}
+
+export default reducer;

086ab37a49eebdc4b618153798ceae89a467f62a
Emmanuel Obonyo <emmyobonyo@gmail.com>
2022-05-29 11:48:27 +0300

    Add user.js file

--- count.js
+++ count.js
@@ -1,48 +1,15 @@
-function changeCount(amount = 1) {
+export function changeCount(amount = 1) {
     return {
         type: "CHANGE_COUNT",
         payload: amount
     }
 }
 
-function countReducer (count = 0, action) {
+export default function countReducer (count = 0, action) {
     switch(action.type) {
         case "CHANGE_COUNT":
             return count + action.payload
         default:
             return count
-    }
-}
-
-function youTubeVideoReducer(youTubeVideo = initialState, action) {
-    switch(action.type) {
-        case "INCREMENT_VIEW_COUNT":
-            return {
-                ...youTubeVideo,
-                viewCount: youTubeVideo.viewCount + 1
-            }
-        case "SET_YOUTUBE_TITLE":
-            return {
-                ...youTubeVideo,
-                title: action.payload
-            }
-        case "UPVOTE_VIDEO":
-            return {
-                ...youTubeVideo,
-                votes: {
-                    ...youTubeVideo.votes,
-                    up: youTubeVideo.votes.up + 1
-                }
-            }
-        case "DOWNVOTE_VIDEO":
-            return {
-                ...youTubeVideo,
-                votes: {
-                    ...youTubeVideo.votes,
-                    down: youTubeVideo.votes.down + 1
-                }
-            }
-        default:
-            return youTubeVideo
     }
 }
--- index.js
+++ index.js
@@ -1,86 +1,17 @@
 const redux = require("redux")
+const {combineReducers, createStore} = redux
+import countReducer from "./count"
+import favoriteThingsReducer from "./favoriteThings"
+import youTubeVideoReducer from "./youTubeVideo"
 
-const initialState = {
-    count: 0,
-    favoriteThings: [],
-    youTubeVideo: {
-        title: "",
-        viewCount: 0,
-        votes: {
-            up: 0,
-            down: 0
-        }
-    }
-}
-console.log(initialState)
+const rootReducer = combineReducers({
+    count: countReducer,
+    favoriteThings: favoriteThingsReducer,
+    youTubeVideo: youTubeVideoReducer
+})
 
-function reducer(state = initialState, action) {
-    switch(action.type) {
-        case "CHANGE_COUNT":
-            return {
-                ...state,
-                count: state.count + action.payload
-            }
-        case "ADD_FAVORITE_THING":
-            return {
-                ...state,
-                favoriteThings: [...state.favoriteThings, action.payload]
-            }
-        case "REMOVE_FAVORITE_THING": {
-            const arrCopy = [...state.favoriteThings]
-            
-            const updatedArr = state.favoriteThings.filter(thing => thing.toLowerCase() !== action.payload.toLowerCase())
-            return {
-                ...state,
-                favoriteThings: updatedArr
-            }
-        }
-        case "INCREMENT_VIEW_COUNT":
-            return {
-                ...state,
-                youTubeVideo: {
-                    ...state.youTubeVideo,
-                    viewCount: state.youTubeVideo.viewCount + 1
-                }
-            }
-        case "SET_YOUTUBE_TITLE":
-            return {
-                ...state,
-                youTubeVideo: {
-                    ...state.youTubeVideo,
-                    title: action.payload
-                }
-            }
-        case "UPVOTE_VIDEO":
-            return {
-                ...state,
-                youTubeVideo: {
-                    ...state.youTubeVideo,
-                    votes: {
-                        ...state.youTubeVideo.votes,
-                        up: state.youTubeVideo.votes.up + 1
-                    }
-                }
-            }
-        case "DOWNVOTE_VIDEO":
-            return {
-                ...state,
-                youTubeVideo: {
-                    ...state.youTubeVideo,
-                    votes: {
-                        ...state.youTubeVideo.votes,
-                        down: state.youTubeVideo.votes.down + 1
-                    }
-                }
-            }
-        default:
-            return state
-    }
-}
-
-const store = redux.createStore(reducer)
-
+const store = createStore(rootReducer)
 store.subscribe(() => {
     console.log(store.getState())
 })
-
+export default store
